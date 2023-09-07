f38161dacbe3758a1cb9849ce02a094dedbca614
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-07 12:26:17 -0700

    Adding file: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Program prints a specific message using printf and returns 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Prints the specific message*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/* Returns 0*/
+	return (0);
+}
