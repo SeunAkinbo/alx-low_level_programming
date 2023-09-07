f9e5da53e91989e3d82b437ba27362fec648da62
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-07 12:05:41 -0700

    Adding file: 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@
 int main(void)
 {
     /* Print the message using the puts function */
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
     /* Return 0 to indicate successful completion */
     return (0);
