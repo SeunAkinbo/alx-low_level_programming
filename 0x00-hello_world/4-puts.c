95db6de4bcf82236c8bb5df6f3d62d46af8871d1
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-07 11:01:17 -0700

    Adding file: 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the message "Programming is like
+ * building a multilingual puzzle" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     puts("Programming is like building a multilingual puzzle");
-    return(0);
+    return (0);
 }
+
