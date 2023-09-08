4bdb3c2cbf94dda17e5a4e159f96166308b60dcc
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-08 12:30:35 -0700

    Adding files: 5-print_numbers & 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Program prints single digit numbers 0 to 9
+ *
+ * Return: 0 (Success)
+ **/
+
+
+int main(void)
+{
+
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
