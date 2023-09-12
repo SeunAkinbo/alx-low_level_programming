114d8a83e578a9fc40d7b35f77dd2b2d6e4a7414
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-12 07:19:18 -0700

    Adding file that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description - A program that prints _putchar.
+ *
+ * Return: 0 (Success)
+ **/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
