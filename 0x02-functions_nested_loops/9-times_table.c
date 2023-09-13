bb04e88beee303026251c416bac37a8434befbb5
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-12 21:23:50 -0700

    Adding file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Function outputs the multiplication table 0 - 9
+ *
+ * Return: Void
+ **/
+
+void times_table(void)
+{
+	int num_1, num_2;
+
+	for (num_1 = 0; num_1 < 10; num_1++)
+	{
+		for (num_2 = 0; num_2 < 10; num_2++)
+		{
+			int mul;
+
+			mul = num_1 * num_2;
+			printf("%d, ", mul);
+
+/*			mul = num_1 * num_2;
+			_putchar('0' + (mul / 10));
+			_putchar('0' + (mul % 10));
+			_putchar(',');
+			_putchar(' ');
+*/
+		}
+		printf("\n");
+	}
+}
