51b28134a9da945e3ee230f303bc339af0bf3621
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-12 19:23:12 -0700

    Adding files that computes absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _abs - Function computes theabsolute value of a number
+ *
+ * @num: Takes an integer argument
+ *
+ * Return: Always 0
+ **/
+
+
+int _abs(int num)
+{
+	return (abs(num));
+}
