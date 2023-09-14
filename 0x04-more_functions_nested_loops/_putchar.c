38823f2aab2c62320f035c2d59a2c035160a493d
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-14 14:49:55 -0700

    Print numbers using putchar

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Prints characters using putchar
+ *
+ * @c: Integer c
+ *
+ * Return: Void
+ **/
+
+
+void _putchar(char c)
+{
+	putchar(c);
+}
