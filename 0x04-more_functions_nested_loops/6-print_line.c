eb177088e547e83ec8e62504a03163e19c4e8848
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-14 16:31:23 -0700

    Adding file: print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,18 +12,14 @@
 void print_line(int n)
 {
 	if (n <= 0)
-	{
-		_putchar('$');
 		_putchar('\n');
-	}
 	else
 	{
-		while (n >= 0)
+		while (n > 0)
 		{
 			_putchar('_');
 			n--;
 		}
-		_putchar('$');
 		_putchar('\n');
 	}
 }
