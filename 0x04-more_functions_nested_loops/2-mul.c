84e15ba54983d634f2411a304c23ae72a133cf6e
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-14 13:56:31 -0700

    Adding files mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplies 2 numbers
+ *
+ * @a: First Integer
+ * @b: Second Integer
+ *
+ * Return: Always 0
+ **/
+
+int mul(int a, int b)
+{
+	printf("%d", a * b);
+
+	return (0);
+}
