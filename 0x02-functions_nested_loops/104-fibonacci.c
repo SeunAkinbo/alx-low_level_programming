422755d96317e346960f112bcfb1e286b2a82ac4
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-14 08:01:01 -0700

    Adding file that computes the first 100 fibnanci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Output the Fibonacci numbers
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+	unsigned long int term1, term2, next;
+	unsigned long int a, b, sum;
+	int i;
+
+	term1 = 1;
+	term2 = 2;
+
+	printf("%lu, %lu, ", term1, term2);
+
+	for (i = 1; i <= 96; i++)
+	{
+		if (i % 2 == 1)
+		{
+			a = term1 + term2;
+			printf("%lu", a);
+			term1 = a;
+		}
+		else
+		{
+			b = term1 + term2;
+			printf("%lu", b);
+			term2 = b;
+		}
+
+		if (i != 96)
+			printf(", ");
+	}
+
+	sum = term1 + term2;
+	printf(", %lu\n", sum);
+
+	return (0);
+}
+
