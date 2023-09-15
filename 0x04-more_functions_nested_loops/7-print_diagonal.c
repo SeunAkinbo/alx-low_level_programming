f420910bcc338dad889f14acabed083db26e70f5
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-14 18:45:38 -0700

    Print Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,15 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 		_putchar('\n');
-	else
+	for (i = 0; i < n; i++)
 	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j <= i; j++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
+		for (j = 0; j <= i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
