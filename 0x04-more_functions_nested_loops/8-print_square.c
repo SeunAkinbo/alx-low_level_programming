725ac75376e861e0c3e08441e285c775ceb2b945
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-14 17:34:08 -0700

    File that generates square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints Square
+ *
+ * @size: Integer
+ *
+ * Return: Null
+ **/
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
