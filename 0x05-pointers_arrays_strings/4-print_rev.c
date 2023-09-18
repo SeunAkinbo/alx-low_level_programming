f7eed027cc6f1d950656805ce3d8881ad3c470b6
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-18 16:38:11 -0700

    Adding File 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * print_rev - Prints string in reverse
@@ -13,7 +14,7 @@ void print_rev(char *s)
 {
 	int counter, limit;
 
-	limit = sizeof(s) - 1;
+	limit = strlen(s);
 	for (counter = limit - 1; counter >= 0; counter--)
 		_putchar(s[counter]);
 	_putchar('\n');
