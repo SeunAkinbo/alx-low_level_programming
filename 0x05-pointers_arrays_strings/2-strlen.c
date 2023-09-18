ffd129366004b9175849b44447a6f9bc7b15fc51
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-18 15:07:55 -0700

    Adding Fiile 2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _strlen - Functionmchecks for String lenght
+ *
+ * @s: String Argument
+ *
+ * Return: Lenght of String
+ **/
+
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
