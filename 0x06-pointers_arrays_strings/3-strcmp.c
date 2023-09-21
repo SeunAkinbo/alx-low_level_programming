526dfd85e48d3ca3278ea3599ec71ac46de8d533
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-21 09:13:37 -0700

    Adding File 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compares Two Strings
+ *
+ * @s1: First String Argument
+ * @s2: Second String Argument
+ *
+ * Return: Difference between the Strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+
+}
