847d8d6f1d6bc1938009281ee35fcfa321febce9
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-25 16:49:44 -0700

    Adding Files set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value to the pointer character
+ *
+ * @s: Pointer of a Pointer
+ * @to: Pointer
+ *
+ * Return: Always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
