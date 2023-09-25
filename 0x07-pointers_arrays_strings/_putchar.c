20e0ab239866081c65325c7b7c5bf6eabacc82c4
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-25 16:01:50 -0700

    Adding Files print_chessboard

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - Prints Character using putchar
+ *
+ * @c: Character Argument
+ *
+ * Return: Always 0
+ */
+
+void _putchar(char c)
+{
+	putchar(c);
+}
