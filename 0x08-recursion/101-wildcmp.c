8a8c883415dfca908073cf2884ca50a85c24534c
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-09-28 17:20:58 -0700

    Adding File wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * wildcmp - Compare two strings with wildcard character '*'.
+ * @s1: The first string.
+ * @s2: The second string with a wildcard character '*'.
+ *
+ * Return: 1 if the strings can be considered identical, 0 otherwise.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
+
