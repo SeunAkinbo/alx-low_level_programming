bb6677302848d47adf534e6e338fc65e35d2bbb8
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-02 17:52:05 -0700

    Adding Static Files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _memset - Checks Memory
+ * @s: String
+ * @b: Char
+ * @n: Int
+ * Return: String
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - strcat
+ * @dest: String
+ * @src: String
+ * Return: String
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
