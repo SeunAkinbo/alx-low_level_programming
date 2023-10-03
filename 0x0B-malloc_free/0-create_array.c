b048013128b639a6e74fb5bf1d21c19b0dc70845
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-03 11:42:45 -0700

    Adding malloc test files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <>
+
+/**
+ * create_array - Creates an array of character
+ * @size: Integer Argument
+ * @c: Character Argument
+ * Return: A pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	
+}
