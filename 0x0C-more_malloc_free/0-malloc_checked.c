5ca59e6885299d04e30ef63d642ab728655fc630
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-07 16:37:06 -0700

    Adding file: 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
  * malloc_checked - Allocates memory using malloc
  * @b: Unsigned Integer Argument
@@ -10,13 +10,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
+
+	ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		exit(98);
-	}
 
 	return (ptr);
 }
