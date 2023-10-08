e3050c7f1b7d93b7aced37d9be9a2c327142a4ae
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-08 05:03:23 -0700

    Adding files Header and 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		new_ptr = malloc(new_size);
 
-        	if (new_ptr == NULL)
+		if (new_ptr == NULL)
 			return (NULL);
 
 		return (new_ptr);
