ecae030aa45831dce6380d5c25c02b59c796da46
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-08 06:36:48 -0700

    Adding file 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 int multiply(char *num1, char *num2);
 
 int is_digit(char *str);
@@ -16,24 +16,38 @@ int is_digit(char *str);
 int main(int argc, char *argv[])
 {
 	int result;
+	unsigned int i;
+	char result_string[12];
+	char *err = "Error";
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		for (i = 0; i < 6; i++)
+			_putchar(err[i]);
+
+		_putchar('\n');
 		return (98);
 	}
+
 	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
-		printf("Error\n");
+		for (i = 0; i < 6; i++)
+			_putchar('E');
+
+		_putchar('\n');
 		return (98);
 	}
 
 	result = multiply(argv[1], argv[2]);
+	sprintf(result_string, "%d", result);
 
-	printf("%d\n", result);
+	for (i = 0; i < strlen(result_string); i++)
+		_putchar(result_string[i]);
 
+	_putchar('\n');
 	return (0);
 }
+
 
 /**
  * multiply - Multiplies two numbers.
