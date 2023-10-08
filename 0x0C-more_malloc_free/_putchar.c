0fdeac3538cffa7bb9831e6aea8b81365067a9a3
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-08 06:03:12 -0700

    Adding files: /**

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _putchar - Outputs character
+ * @c: Character Argument
+ * Return: Integer
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
