30c8a551b464df8a05ddc408e26c2456a6ced519
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-10 12:46:26 -0700

    Ading Structure file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Type definition dog
+ * @name: String name of the dog
+ * @age: Float age of the dog
+ * @owner: String owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
