fe459517ffae4e9ea61e27dd5ec2a7b977a03e2e
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-10 14:06:46 -0700

    Adding file: 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,7 +6,7 @@
  * print_dog - Prints a struct dog
  * @d: A pointer to struct dog
  *
- * Return: Always 0
+ * Return: Always NULL
  */
 
 void print_dog(struct dog *d)
@@ -19,10 +19,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-	if (d->age == 0)
-		printf("(nil)\n");
-	else
-		printf("Age: %.6f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("(nil)");
