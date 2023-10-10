d4629e91bb75a21e52afc148ab9ec8f020accd03
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-10 13:18:43 -0700

    Adding file 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes type struct dog
+ * @d: Pointer to the struct dog
+ * @name: String Name of the dog
+ * @age: Float Age of the dog
+ * @owner: String owner of the dog
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
