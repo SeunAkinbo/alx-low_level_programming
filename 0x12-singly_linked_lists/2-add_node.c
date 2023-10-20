98f3830745b3ecead0c50cd0019597a37a0e6221
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-10-19 21:32:35 -0700

    Adding file - 0-print_list.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: The address of the head of the linked list.
+ * @str: The string to be added to the new node.
+ * Return: The address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node_end(list_t *head, const char *str)
+{
+	list_t *new_node;
+	list_t *last_node;
+	size_t len = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	len = strlen(str);
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = len;
+	new_node->next = NULL;
+
+	if (head == NULL)
+		head = new_node;
+	else
+	{
+		last_node = head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
+	return (head);
+}
