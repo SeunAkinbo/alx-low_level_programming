bc8218939f84bf3e8a91ab41902b8be40d45bf52
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-07 11:08:47 -0800

    Adding File: 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,31 +1,5 @@
 #include "lists.h"
 
-/**
- * creates_node - Creates a new node
- * @n: The value of the node integer variable
- * Return: A new node on success or NULL otherwise
- *
- * Description: The function  creates a new node, assigns
- *		a value to the integer variable and returns
- *		the new node
- **/
-
-
-dlistint_t *creates_nodes(const int n)
-{
-	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
-
-	return (new);
-}
-
 
 /**
  * insert_dnodeint_at_index - Inserts node at an index in a doubly linked list
@@ -63,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new = add_dnodeint_end(h, n);
 	else
 	{
-		new = creates_nodes(n);
+		new = create_node(n);
 		if (!new)
 			return (NULL);
 
