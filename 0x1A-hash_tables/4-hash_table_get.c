9a6829fd68fe6d16b4a2fcb398a57e4e6893086b
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-22 12:12:30 -0800

    Adding File: 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_get - Retrives a value associated with a key
+ * @ht: The hash table
+ * @key: The String item being searched for
+ * Return: Value associated with key or NULL if none
+ **/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			return (ht->array[i]->value);
+		}
+	}
+
+	return (NULL);
+}
