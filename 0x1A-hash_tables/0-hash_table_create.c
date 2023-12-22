c2bd37e031dcb70b0b45113a2d643bf325d74286
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-22 07:06:19 -0800

    Adding File: 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,17 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
-	{
-		fprintf(stderr, "Error: cannot malloc");
-		exit(EXIT_FAILURE);
-	}
+		return (NULL);
 
 	array = malloc(sizeof(hash_node_t));
 	if (!array)
-	{
-		fprintf(stderr, "Error: cannot malloc");
-		exit(EXIT_FAILURE);
-	}
+		return (NULL);
 
 	hash_table->size = size;
 	hash_table->array = &array;
