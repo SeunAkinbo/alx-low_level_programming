29a3cefe4c7e4e53504d74fa97aea8a2d3357021
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-22 13:57:13 -0800

    Adding File: 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -28,15 +28,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		node = node->next;
 	}
 
-	/*
-	for (i = index; ht->array[i]; i++)
-	{
-		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			return (ht->array[i]->value);
-		}
-	}
-	*/
-
 	return (NULL);
 }
