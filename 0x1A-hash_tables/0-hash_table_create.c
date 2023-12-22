cf031ba0963ad415e5d82ce3bcb2f1f749be8411
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-22 07:20:02 -0800

    Adding File: 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,30 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	hash_node_t *array = NULL;
+	hash_table_t *hash_table = NULL;
+	unsigned int i = 0;
 
+	/**Allocates memory to the hash table**/
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
-	array = malloc(sizeof(hash_node_t));
-	if (!array)
+	/**Allocates memory to the array**/
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	if (!hash_table->array)
+	{
+		free(hash_table);
 		return (NULL);
+	}
+	
+	/**Initializes the array and assigns NULL values to it**/
+	for (; i < size; i++)
+		hash_table->array[i] = NULL;
 
 	hash_table->size = size;
-	hash_table->array = &array;
 
 	return (hash_table);
 }
