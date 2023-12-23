49a7634bdb6e205e24a490b0994af52a67e37b55
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-22 17:48:38 -0800

    PHP

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,32 +1,39 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table
- * @ht: Hash table
- * Return: void
- **/
+ * free_node - Frees a hash node and its content
+ * @node: The hash node to be freed
+ */
+void free_node(hash_node_t *node)
+{
+    free(node->key);
+    free(node->value);
+    free(node);
+}
 
+/**
+ * hash_table_delete - Deletes a hash table and its nodes
+ * @ht: The hash table to be deleted
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int idx;
-	hash_node_t *node, *temp;
+    unsigned long int i;
+    hash_node_t *current, *temp;
 
-	if (!ht)
-		return;
+    if (ht == NULL)
+        return;
 
-	for (idx = 0; idx < ht->size; idx++)
-	{
-		node = ht->array[idx];
-		while (node)
-		{
-			temp = node->next;
-			free(node->key);
-			free(node->value);
-			free(node);
-			node = temp;
-		}
-	}
+    for (i = 0; i < ht->size; i++)
+    {
+        current = ht->array[i];
+        while (current != NULL)
+        {
+            temp = current->next;
+            free_node(current);
+            current = temp;
+        }
+    }
 
-	free(ht->array);
-	free(ht);
+    free(ht->array);
+    free(ht);
 }
---
+++ 0x1A-hash_tables/6.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash table
+ * Return: void
+ **/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
