a3132c178522ea7d676347ccf0d08b52174d8195
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-22 17:22:21 -0800

    Adding File: 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,23 +12,21 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *temp;
 
 	if (!ht)
-		return
+		return;
 
 	for (idx = 0; idx < ht->size; idx++)
 	{
-		if (!ht->array[idx])
+		node = ht->array[idx];
+		while (node)
 		{
-			node = ht->array[idx];
-			while (node)
-			{
-				temp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = temp;
-			}
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
 		}
 	}
+
 	free(ht->array);
 	free(ht);
 }
