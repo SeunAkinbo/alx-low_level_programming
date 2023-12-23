d7c0666c0467a541eeb9b1266a25b02821195abf
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-23 12:04:59 -0800

    Code Test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,10 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			temp->value = strdup(value);
 			if (temp->value == NULL)
 				return (0);
-			return (1);
-		}
-		temp = temp->next;
-	}
+			return (1); }
+		temp = temp->next; }
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
@@ -41,17 +39,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return (0);
-	}
+		return (0); }
 
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
-		return (0);
-	}
-
+		return (0); }
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
