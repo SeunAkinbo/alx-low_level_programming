f517c3dbfa06e6ec7f73eabb87f99f846aaa83fe
Oluwaseun Akinbo <seunakinbo@gmail.com>
2023-12-24 22:00:10 -0800

    Updating File

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table
- * @size: The size of the hash table
- * Return: The created hash table
- **/
+* hash_table_create - Creates a hash table
+* @size: The size of the hash table
+* Return: The created hash table
+**/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
