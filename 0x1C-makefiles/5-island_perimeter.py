4f9bd1e579a7c99b782d4b2dae00fec63fc0552b
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-21 13:17:34 -0800

    Adding File 5-island_perimeter:

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """
+    Function returns the perimeter of the island
+
+    Args:
+    - grid: List of list of integers
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
