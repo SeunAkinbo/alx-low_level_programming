f0b1e3a53168e470ceb511bd166d4fdd4fddd19a
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-21 08:23:32 -0800

    Adding File 0-Makefile.c

---
+++ 0x1C-makefiles/files.h
@@ -0,0 +1,9 @@
+#ifndef FILES_H
+#define FILES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_school(void);
+
+#endif /**FILES_H**/
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "files.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS
+ **/
+
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
